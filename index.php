4aa11a98b83d533165f69babe71668d2f51f45e3
sua-no <sua953@naver.com>
2020-07-04 18:47:52 +0900

    파비콘 추가

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>네이버 : 로그인</title>
+    <link rel="shortcut icon" href="img/favicon.ico">
     <link rel="stylesheet" href="css/main.css">
 
 </head>
